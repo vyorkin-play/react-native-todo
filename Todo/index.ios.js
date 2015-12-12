712a3e671f1017543d4ef1f74b361d4ab723c447
Vasiliy Yorkin <vasiliy.yorkin@gmail.com>
2015-12-13 02:38:44 +0300

    Add Image

--- Todo/index.ios.js
+++ Todo/index.ios.js
@@ -8,24 +8,32 @@ var React = require('react-native');
 var {
   AppRegistry,
   StyleSheet,
+  Image,
   Text,
   View,
 } = React;
 
+var MOCKED_MOVIES_DATA = [
+  {
+    title: 'Title',
+    year: '2015',
+    posters: {
+      thumbnail: 'http://i.imgur.com/UePbdph.jpg'
+    }
+  }
+];
+
 var Todo = React.createClass({
   render: function() {
+    var movie = MOCKED_MOVIES_DATA[0];
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+        <Text>{movie.title}</Text>
+        <Text>{movie.year}</Text>
+        <Image
+          source={ { uri: movie.posters.thumbnail } }
+          style={styles.thumbnail}
+        />
       </View>
     );
   }
@@ -38,16 +46,10 @@ var styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  thumbnail: {
+    width: 53,
+    height: 81
+  }
 });
 
 AppRegistry.registerComponent('Todo', () => Todo);
